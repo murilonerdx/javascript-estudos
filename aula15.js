5e99cfcfc7dc1fc8e7c9a917d34b3e9389031d13
Murilonerdx <mu-silva@outlook.com>
2021-03-24 13:36:07 -0300

    Exercicios number e math

---
+++ aula15.js
@@ -0,0 +1,10 @@
+let num1 = 9.55092;
+let num2 = Math.floor(num1); //arredondar pra baixo.
+let num3 = Math.ceil(num1); //arredonda pra cima
+let num4 = Math.round(num1); //Apenas arredonda
+console.log(num2, num3, num4);
+
+console.log(Math.max(1,5,1,6,77,13,613,713,125,6126,1727,1256)) // Vai voltar o maior numero
+console.log(Math.random()); //Gerar numero aleatorio
+let num5 = Math.random() * (10 -5 ) + 5; //Gerar numero de 5 e 10;
+console.log(Math.round(num5)); // gerar numero aleatorio
