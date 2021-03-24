f3dec4086b0df9a60afe4ab21bb83caf480efb4f
Murilonerdx <mu-silva@outlook.com>
2021-03-24 16:32:02 -0300

    Exercicio 2

---
+++ Javascript Logica de Programação/exercicios-2/assets/js/main.js
@@ -0,0 +1,62 @@
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+
+function getDiaSemana(diaSemana) {
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda-feira';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça-feira';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta-feira';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta-feira';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta-feira';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sabado';
+            return diaSemanaTexto;
+    }
+}
+
+
+function getNomeMes(mesDoAno) {
+    let nomeMes;
+    switch (mesDoAno) {
+        case 0:
+            nomeMes = 'Janeiro';
+            return nomeMes;
+        case 1:
+            nomeMes = 'Fevereiro';
+            return nomeMes;
+        case 2:
+            nomeMes = 'Março';
+            return nomeMes;
+        case 3:
+            nomeMes = 'Abril';
+            return nomeMes;
+        case 4:
+            nomeMes = 'Maio';
+            return nomeMes;
+        case 5:
+            nomeMes = 'Junho';
+            return nomeMes;
+        case 6:
+            nomeMes = 'Julho';
+            return nomeMes;
+    }
+}
+
+
+
+
+h1.innerHTML = getDiaSemana(data.getDay());
+h1.innerHTML += `, ${data.getDay()} de ${getNomeMes(data.getMonth())} de ${data.getFullYear()}`;
