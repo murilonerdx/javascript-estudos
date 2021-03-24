75e98076885e2447057e96daa17b6cddf96bb4fe
Murilonerdx <mu-silva@outlook.com>
2021-03-24 14:45:46 -0300

    Operadores de comparação

---
+++ Javascript Logica de Programação/aula1-operadores.js
@@ -0,0 +1,19 @@
+// Operadores de compração
+// > maior que
+// >= maior que ou igual a
+// < menor que
+// <= menor ou igual a
+// == igualdade (valor)
+// === igualdade ( tipo do valor)
+// != diferente que
+// !== diferente do tipo do valor
+
+console.log(10 > 5); //true
+console.log(10 >= 5); //true
+console.log(10 < 5);
+console.log(10 <= 5);
+console.log(10 == 5);
+console.log(10 === 5);
+console.log(10 !== 5); //true
+console.log(10 != 5); //true
+
