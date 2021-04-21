dc25b1527f2ed752a4555bc07eb77b888d51a0ef
Murilonerdx <mu-silva@outlook.com>
2021-04-21 15:17:14 -0300

    Interface

--- TypeScript e Orientação a Objetos em Javascript/aula-9/Concessionaria.ts
+++ TypeScript e Orientação a Objetos em Javascript/aula-9/Concessionaria.ts
@@ -1,10 +1,14 @@
-export class Concessionaria{
+import { ConcessionariaInterface } from './ConcessionariaInterface';
+export class Concessionaria implements ConcessionariaInterface{
     private endereco!: string;
     private listaDeCarro:any;
 
     constructor(endereco:string,listaDeCarro:any){
         this.endereco=endereco;
         this.listaDeCarro=listaDeCarro;
+    }
+    fornecerHorarioDeFuncionamento(): void {
+        console.log("De segunda a sexta das 08:00 as 18:00");
     }
 
     public fornecerEndereco():string{
