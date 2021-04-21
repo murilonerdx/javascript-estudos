d21c130c57f085bd600a4807105eec3c3de4be6c
Murilonerdx <mu-silva@outlook.com>
2021-04-21 13:40:05 -0300

    Inferencia de tipos

---
+++ TypeScript e Orientação a Objetos em Javascript/aula-3/app.js
@@ -0,0 +1,5 @@
+var mensagem = "Seja bem vindo!!";
+var temporadasFriends = 10;
+var estudandoAngular = true;
+var listaDeFrutas = ["Pera", "Uva", "Maça"];
+var notasDasProvas = [7.5, 8, 9];
---
+++ TypeScript e Orientação a Objetos em Javascript/aula-3/app.ts
@@ -0,0 +1,9 @@
+let mensagem:string = "Seja bem vindo!!";
+let temporadasFriends:number = 10;
+let estudandoAngular:boolean = true;
+let listaDeFrutas:Array<string> = ["Pera","Uva","Maça"];
+let notasDasProvas:Array<number> = [7.5,8,9];
+
+//se você tentar modificar para um tipo que não seja do tipo especificado ele da erro.
+
+//Você consegue definir o tipo dela
