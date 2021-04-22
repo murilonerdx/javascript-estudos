cac95da7c67624f7993bfc6b94ef0ee837c3c85d
Murilonerdx <mu-silva@outlook.com>
2021-04-22 17:19:27 -0300

    Gerando component progresso e tentativas ng g c tentativas --skip-tests=true

--- Introdução ao Angular 4/Projetos/app1/src/app/app.module.ts
+++ Introdução ao Angular 4/Projetos/app1/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopoComponent} from './topo/topo.component';
-import { PainelComponent } from './painel/painel.component'
+import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    PainelComponent
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
