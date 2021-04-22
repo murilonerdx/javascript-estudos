de3a71a9788d58c3e586d44306772dd4ae4ec5b1
Murilonerdx <mu-silva@outlook.com>
2021-04-22 17:02:43 -0300

    Component Styles

--- Introdução ao Angular 4/Projetos/app1/src/app/topo/topo.component.ts
+++ Introdução ao Angular 4/Projetos/app1/src/app/topo/topo.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.css']
   
 })
 export class TopoComponent {}
