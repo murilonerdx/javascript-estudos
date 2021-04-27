b6bf87cb14755ee57c7ba4788e8e1385de495f6d
Murilonerdx <mu-silva@outlook.com>
2021-04-27 15:25:44 -0300

    String interpolation, biding

--- Introdução ao Angular 4/Projetos/app1/src/app/topo/topo.component.ts
+++ Introdução ao Angular 4/Projetos/app1/src/app/topo/topo.component.ts
@@ -9,4 +9,6 @@ import { Component } from '@angular/core'
     // styles: ['.exemplo {color: red}']
     styleUrls: ['./topo.component.css']
 })
-export class TopoComponent { }
+export class TopoComponent {
+    public titulo: string = "Aprendendo Inglês";
+ }
