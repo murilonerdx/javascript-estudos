f56e12feeb64a3698ab415521e979732da84a04e
Murilonerdx <mu-silva@outlook.com>
2021-05-03 12:45:55 -0300

    Criando class Frase

---
+++ Introdução ao Angular 4/Projetos/app1/src/app/shared/frase.model.ts
@@ -0,0 +1,9 @@
+export class Frase{
+  public fraseEng: string;
+  public frasePTBr: string;
+
+  constructor(fraseEng:string,frasePtBr:string){
+    this.fraseEng = fraseEng;
+    this.frasePtBr = frasePtBr;
+  }
+}
