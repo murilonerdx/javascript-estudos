ac194fbe9e6b3bb4133eab7527f8d480a6d1f815
Murilonerdx <mu-silva@outlook.com>
2021-05-03 15:11:59 -0300

    Criando variaveis estaticas

--- Introdução ao Angular 4/Projetos/app1/src/app/painel/frases-mock.ts
+++ Introdução ao Angular 4/Projetos/app1/src/app/painel/frases-mock.ts
@@ -1,6 +1,6 @@
-import { Frase } from '../shared/frase.model';
+import { Frase } from '../shared/frase-model';
 
-const FRASES: Frase[] = [
+export const FRASES: Frase[] = [
   { fraseEng: 'I like to learn', frasePTBr: 'Eu gosto de aprender' },
   { fraseEng: 'I watch tv', frasePTBr: 'Eu assisto tv' },
   { fraseEng: 'How are you?', frasePTBr: 'Como vai você' },
