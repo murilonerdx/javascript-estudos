790f1bc02877722c029697796c3e7015362c36a6
Murilonerdx <mu-silva@outlook.com>
2021-05-03 16:31:46 -0300

    Verificando resposta

--- Introdução ao Angular 4/Projetos/app1/src/app/painel/painel.component.ts
+++ Introdução ao Angular 4/Projetos/app1/src/app/painel/painel.component.ts
@@ -10,14 +10,31 @@ import { FRASES } from './frases-mock';
 export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES;
   public instrucao: string = "Traduza a frase:";
+  public resposta: string;
+  public rodada: number = 0;
+  public rodadaFrase;
 
-  constructor() { }
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase);
+  }
 
   ngOnInit() {
   }
 
-  atualizaResposta(resposta:Event): void {
-    console.log((<HTMLInputElement>resposta.target).value);
+  atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  verificaResposta(): void {
+    if (this.rodadaFrase.frasePTBr == this.resposta) {
+      alert("Resposta certa");
+      this.rodada++;
+      this.rodadaFrase = this.frases[this.rodada];
+    } else {
+      alert("Resposta Errada")
+    }
+    
   }
 
 }
