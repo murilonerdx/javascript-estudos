3cceb6ac0bec7ab774469ee063d289ef2864593b
Murilonerdx <mu-silva@outlook.com>
2021-05-17 22:09:51 -0300

    Exibindo ofertas com base no serviço ofertasService

--- Introdução ao Angular 4/Projetos/app2/src/app/home/home.component.ts
+++ Introdução ao Angular 4/Projetos/app2/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertaService} from '../oferta.service'
-
+import { Oferta  } from '../oferta.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,10 +9,12 @@ import { OfertaService} from '../oferta.service'
 })
 export class HomeComponent implements OnInit {
 
+  public oferta: Oferta[]
   constructor(public service:OfertaService) { }
 
   ngOnInit() {
-    console.log(this.service.getOfertas);
+    this.oferta = this.service.getOfertas()
+    console.log(this.oferta)
   }
 
 }
---
+++ Introdução ao Angular 4/Projetos/app2/src/app/oferta.model.spec.ts
@@ -0,0 +1,7 @@
+import { Oferta } from './oferta.model';
+
+describe('Oferta', () => {
+  it('should create an instance', () => {
+    expect(new Oferta()).toBeTruthy();
+  });
+});
