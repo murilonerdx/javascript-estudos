80e59164d4762ed2439f2cc9753ee312ce698769
murilonerdx <mu-silva@outlook.com>
2022-03-21 23:31:19 -0300

    Aprendendo sobre modulos

--- NodeJS/estudos/aula-1/app.js
+++ NodeJS/estudos/aula-1/app.js
@@ -1,9 +1,21 @@
 const {nome, sobrenome, falaNome} = require('./mod1');
 const { Pessoa } = require('./mod2');
+const multiplicacao = require('./mod3');
+const Cachorro = require('./mod4')
 
 const mod1 = require('./mod1');
-
+const cachorrinho = new Cachorro("Kiba");
 const p1 = new Pessoa('Luiz');
 
-console.log(p1);
+console.log(multiplicacao(2,2));
+console.log(cachorrinho.latir());
+
+console.log(__filename);
+console.log(__dirname);
+
+const path = require('path');
+console.log(path.resolve(__dirname));
+
+console.log(path.resolve(__dirname, '../'));
+
 
