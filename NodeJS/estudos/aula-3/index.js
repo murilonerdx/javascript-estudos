60f494dc3f1d3517851d5e1512702aa7cd4a8fd0
murilonerdx <mu-silva@outlook.com>
2022-03-21 23:55:18 -0300

    Listando arquivos com FS e Recursão mutua

---
+++ NodeJS/estudos/aula-3/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+// fs.readdir(path.resolve(__dirname, '../'))
+    // .then(files=>(console.log(files)))
+    // .catch(e=> console.log(e));
+
+async function readdir(rootDir){
+    rootDir = rootDir || path.resolve(__dirname, '../');
+    const files = await fs.readdir(rootDir);
+    walk(files, rootDir);
+}
+async function walk(files, rootDir){
+    for(let file of files){
+        const fileFullPath = path.resolve(rootDir, file);
+        const stats = await fs.stat(fileFullPath);
+        if(stats.isDirectory()){
+            readdir(fileFullPath);
+        } 
+
+        console.log(file, stats.isDirectory());
+    }
+}
+
+readdir();
