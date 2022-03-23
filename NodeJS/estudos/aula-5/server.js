bce42a6ae9f0988fee34024a123a4e43671fc7eb
murilonerdx <mu-silva@outlook.com>
2022-03-23 17:04:18 -0300

    Express req.params, req.query e req.body

--- NodeJS/estudos/aula-5/server.js
+++ NodeJS/estudos/aula-5/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 
-const app = express(0);
+const app = express();
+
+// Tratar informações na query
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/',(req, res) =>{
-    res.send('Hello world!');
+        res.send(`
+        <form action='/' method="POST">
+            Nome do cliente: <input type="text" name="nome">
+            <button> Enviar</button>
+        </form>
+        `);
+});
+
+app.post('/', (req,res)=>{
+    console.log(req.body);
+    res.send(`O que você enviou foi: ${req.body.nome}`);
 })
 
 app.get('/contato',(req, res) =>{
+    res.send('Obrigado por entrar em contato com a gente.');
+})
+
+app.get('/teste/:idUsuarios?/:parametro?',(req, res) =>{
+    console.log(req.query); // ?nome=Murilo&idade=21&nacionalidade=Brasileiro
+    console.log(req.params);
     res.send('Obrigado por entrar em contato com a gente.');
 })
 
