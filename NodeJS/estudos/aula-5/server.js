5ff1f28e8cb8239c221a1993a2a6d71384a36fce
murilonerdx <mu-silva@outlook.com>
2022-03-23 19:10:03 -0300

    Adicioanndo express router e controllers

--- NodeJS/estudos/aula-5/server.js
+++ NodeJS/estudos/aula-5/server.js
@@ -1,34 +1,11 @@
 const express = require('express');
-
 const app = express();
+const routes = require('./routes');
 
-// Tratar informações na query
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-app.get('/',(req, res) =>{
-        res.send(`
-        <form action='/' method="POST">
-            Nome do cliente: <input type="text" name="nome">
-            <button> Enviar</button>
-        </form>
-        `);
-});
-
-app.post('/', (req,res)=>{
-    console.log(req.body);
-    res.send(`O que você enviou foi: ${req.body.nome}`);
-})
-
-app.get('/contato',(req, res) =>{
-    res.send('Obrigado por entrar em contato com a gente.');
-})
-
-app.get('/teste/:idUsuarios?/:parametro?',(req, res) =>{
-    console.log(req.query); // ?nome=Murilo&idade=21&nacionalidade=Brasileiro
-    console.log(req.params);
-    res.send('Obrigado por entrar em contato com a gente.');
-})
-
-app.listen(3000, ()=>{
-    console.log("Servidor rodando na porta 3000");
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
 });
