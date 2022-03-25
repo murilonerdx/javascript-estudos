f2d9edf1ff18098809b519e135b074afb162ad4f
murilonerdx <mu-silva@outlook.com>
2022-03-25 15:33:19 -0300

    Um pouco sobre ejs

--- NodeJS/estudos/aula-5/src/controllers/homeController.js
+++ NodeJS/estudos/aula-5/src/controllers/homeController.js
@@ -6,13 +6,18 @@ const HomeModel = require('../model/HomeModel.js');
 
 exports.paginaInicial = (req, res, next) => {
   // Essa mensagem vai ficar salva no cookie por 7 dias, mesmo fechando a aplicação e o navegador e tudo mais.
-  req.session.usuario = {nome: 'Murilo', logado: true};
-  req.flash('info', 'Informação salva - 1');
-  console.log(req.session.usuario);
+  //req.session.usuario = {nome: 'Murilo', logado: true};
+  //req.flash('info', 'Informação salva - 1');
+    //console.log(req.session.usuario);
 
   //Pegando alguma informação
-  console.log(req.flash('info'));
-  res.render('index');
+    //console.log(req.flash('info'));
+  res.render('index', {
+    titulo: 'Este será o titulo da pagina',
+    tituloStyle: '<span style="color: red;">Alguma coisa vermelha</span>',
+    numeros: [0,1,2,3,4,5,6,7,8,9]
+  });
+  return;
 };
 
 exports.trataPost = (req, res, next) => {
