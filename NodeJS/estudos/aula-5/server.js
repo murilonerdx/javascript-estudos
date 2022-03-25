a833da0b78bbd39916a7b22dba9af27442dbfcdb
murilonerdx <mu-silva@outlook.com>
2022-03-24 22:30:25 -0300

    Express session e flash messages

--- NodeJS/estudos/aula-5/server.js
+++ NodeJS/estudos/aula-5/server.js
@@ -10,10 +10,16 @@ mongoose.connect(process.env.connectionString)
         app.emit('done');
     });
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+
+
 const routes = require('./routes');
 const path = require('path');
 const middleware = require('./src/middleware/middlewareGlobal.js');
-const { middlewareum, middlewaredois, middlewaretres } = require('./src/middleware/variosMiddlewares.js');
+// const { middlewareum, middlewaredois, middlewaretres } = require('./src/middleware/variosMiddlewares.js');
 
 app.on('done', () =>{
     app.listen(3000, () => {
@@ -23,13 +29,30 @@ app.on('done', () =>{
 });
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'meu-secredo',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 3600 * 24 * 7,
+        httpOnly: true
+    },
+    store: MongoStore.create({
+        mongoUrl: process.env.connectionString,
+    })
+});
+
 app.set('views', path.resolve(__dirname, './src/views'))
 app.set('view engine', 'ejs')
 
 app.use(middleware);
-app.use(middlewareum);
-app.use(middlewaredois);
-app.use(middlewaretres);
+app.use(sessionOptions);
+app.use(flash())
+// app.use(middlewareum);
+// app.use(middlewaredois);
+// app.use(middlewaretres);
 app.use(routes);
 
--- NodeJS/estudos/aula-5/src/controllers/homeController.js
+++ NodeJS/estudos/aula-5/src/controllers/homeController.js
@@ -1,10 +1,17 @@
 const HomeModel = require('../model/HomeModel.js');
 
-HomeModel.create({titulo: 'Eu sou lindo', descricao: 'Eu sou muito lindo é verdade!'})
-  .then(dados => console.log(dados))
-  .catch(e => console.log(e));
+// HomeModel.create({titulo: 'Eu sou lindo', descricao: 'Eu sou muito lindo é verdade!'})
+  // .then(dados => console.log(dados))
+  // .catch(e => console.log(e));//
 
 exports.paginaInicial = (req, res, next) => {
+  // Essa mensagem vai ficar salva no cookie por 7 dias, mesmo fechando a aplicação e o navegador e tudo mais.
+  req.session.usuario = {nome: 'Murilo', logado: true};
+  req.flash('info', 'Informação salva - 1');
+  console.log(req.session.usuario);
+
+  //Pegando alguma informação
+  console.log(req.flash('info'));
   res.render('index');
 };
 
