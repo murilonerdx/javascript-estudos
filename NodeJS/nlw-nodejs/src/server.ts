b85bc07919b8573ccd3ca7ff07148a2626cafa74
murilonerdx <mu-silva@outlook.com>
2022-03-26 01:39:53 -0300

    Adicionando projeto inteiro, não fiz ele todo, porque estava com alguns erros que não consegui resolver

--- NodeJS/nlw-nodejs/src/server.ts
+++ NodeJS/nlw-nodejs/src/server.ts
@@ -1,15 +1,31 @@
-import express from "express";
+import "reflect-metadata";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
+app.use(cors());
 
-app.get("/test", (request, response) => {
-    response.send("Olá Mundinho");
-})
+app.use(express.json());
 
-app.post("/test", (request, response) => {
-    response.send("POST: enviando");
-})
+app.use(router);
 
-app.listen(3000, ()=>{
-    console.log("Servidor rodando");
-})
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+);
+
+app.listen(3000, () => console.log("Server is running"));
