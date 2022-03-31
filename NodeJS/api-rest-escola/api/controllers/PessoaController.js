a90bb63bf98c6215195e69f58e1215ac0d629c26
murilonerdx <mu-silva@outlook.com>
2022-03-31 19:46:32 -0300

    Finalizando crud basico

--- NodeJS/api-rest-escola/api/controllers/PessoaController.js
+++ NodeJS/api-rest-escola/api/controllers/PessoaController.js
@@ -9,6 +9,55 @@ class PessoaController {
             return res.status(500).json(error.message);
         }
     }
+
+    static async pegarUmaPessoa(req, res){
+        const {id} = req.params;
+
+        try{
+            const pessoa = await database.Pessoas.findOne({where: {id: Number(id)}});
+            return res.status(200).json(pessoa); 
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async criarPessoa(req, res){
+        const novaPessoa = req.body;
+
+        try{
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            return res.status(200).json(novaPessoaCriada); 
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async atualizarRegistro(req, res){
+        const {id} = req.params;
+        const novaPessoa = req.body;
+
+        try{
+            await database.Pessoas.update(novaPessoa, {where: {id: id}});
+            const pessoaAtualizada = await database.Pessoas.findOne({where: {id: Number(id)}});
+
+            return res.status(200).json(pessoaAtualizada); 
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async deletarRegistro(req, res){
+        const {id} = req.params;
+
+        try{
+            await database.Pessoas.destroy({where: {id: Number(id)}});
+            return res.status(200).json({message: 'Pessoa excluida com sucesso'}); 
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+
 }
 
 module.exports = PessoaController;
--- NodeJS/api-rest-escola/api/routes/pessoasRoutes.js
+++ NodeJS/api-rest-escola/api/routes/pessoasRoutes.js
@@ -4,6 +4,10 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router
-    .get('/pessoas', PessoaController.pegaTodasAsPessoas);
+    .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+    .get('/pessoas/:id', PessoaController.pegarUmaPessoa)
+    .post('/pessoas', PessoaController.criarPessoa)
+    .put('/pessoas/:id', PessoaController.atualizarRegistro)
+    .delete('/pessoas/:id',PessoaController.deletarRegistro)
 
 module.exports = router;
