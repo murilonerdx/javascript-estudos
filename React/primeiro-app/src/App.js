fb7f90c8c4140c5c63e90885a00f66ecc4a43a00
println <murilo_pereira@terceiros.sicredi.com.br>
2023-03-01 21:32:00 -0300

    Primeiro projeto entendendo props

---
+++ React/primeiro-app/src/App.js
@@ -0,0 +1,13 @@
+import Nome from "./component/Nome";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bem vindo ao meu projeto</h1>
+      <Nome aluno='Murilo' idade={22}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/primeiro-app/src/component/Nome.js
@@ -0,0 +1,7 @@
+function Nome({aluno, idade}){
+    return(
+        <span>Bem-vindo: {aluno} você tem {idade} anos</span>
+    )
+}
+
+export default Nome;
