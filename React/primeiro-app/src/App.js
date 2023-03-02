bad2809c758d22de9668662dd1795d36b8dd28a1
println <murilo_pereira@terceiros.sicredi.com.br>
2023-03-01 23:53:39 -0300

    requisições http

--- React/primeiro-app/src/App.js
+++ React/primeiro-app/src/App.js
@@ -1,60 +1,94 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 import { useState, useEffect } from 'react'
 
 function App () {
-  const [input, setInput] = useState('')
-  const [tarefas, setTarefas] = useState([])
-
-  function handleRegister (e) {
-    e.preventDefault()
-
-    alert('Adicionando tarefa')
-    setTarefas([
-      ...tarefas,
-      input
-    ])
-    setInput('')
-  }
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    const tarefaStorage = localStorage.getItem('@tarefa');
-
-    if(tarefaStorage){
-      setTarefas(JSON.parse(tarefaStorage))
+    async function fetchData () {
+      const response = await fetch(
+        'https://sujeitoprogramador.com/rn-api/?api=posts'
+      )
+      const jsonData = await response.json()
+      setData(jsonData)
     }
-    
+
+    async function get () {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+
+    async function post () {
+      fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'foo',
+          body: 'bar',
+          userId: 1
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+
+    async function put () {
+      fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PUT',
+        body: JSON.stringify({
+          id: 1,
+          title: 'foo',
+          body: 'bar',
+          userId: 1
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+
+    async function deletePost () {
+      fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE'
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+
+    fetchData()
   }, [])
 
-  useEffect(() => {
-    localStorage.setItem('@tarefa', JSON.stringify(tarefas));
-  }, [tarefas])
-
-  
-
   return (
-    <div>
-      <form onSubmit={handleRegister} className='m-2 '>
-        <label>Nome da tarefa: </label>
-        <br />
-        <input
-          className='form-control'
-          value={input}
-          onChange={e => setInput(e.target.value)}
-        />
-        <button className='btn btn-primary' type='submit'>
-          Adicionar tarefa
-        </button>
-      </form>
+    <div className='container'>
+      <header>
+        <strong>React Nutri</strong>
+      </header>
 
-      <div className='m-2'>
-        {tarefas.map(tarefa => (
-          <ul>
-            <li key={tarefa}>
-              <span>{tarefa}</span>
-            </li>
-          </ul>
-        ))}
-      </div>
+      {data.map(item => {
+        return (
+          <article key={item.id} className='post'>
+            <h1>Categoria: {item.categoria}</h1>
+            <strong className='titulo'>{item.titulo}</strong>
+
+            <img src={item.capa} alt={item.titulo} className='capa'/>
+            <p className='subtitulo'>
+              {item.subtitulo}
+            </p>
+
+            <button  className="botao" type="button" value="">Acessar pagina</button>
+          </article>
+        )
+      })}
     </div>
   )
 }
