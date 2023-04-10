54593ba487bedc77f6cce6aeb4f734d794bab11b
println <murilo_pereira@terceiros.sicredi.com.br>
2023-04-10 14:52:09 -0300

    Pagina de cadastro

--- Next.js/sujeiro-pizza/frontend/pizzaria/pages/signup/index.tsx
+++ Next.js/sujeiro-pizza/frontend/pizzaria/pages/signup/index.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import Head from 'next/head'
-import logoImg from'../public/logo.svg'
-import style from '../styles/home.module.scss'
+import Head from "next/head";
+import style from "@/styles/home.module.scss";
+import Image from "next/image";
+import logoImg from "@/public/logo.svg";
 import {Input} from "@/components/ui/Input";
 import {Button} from "@/components/ui/Button";
 import Link from "next/link";
 
-export default function Home() {
+export default function Signup() {
 
     return (
         <>
@@ -18,17 +18,20 @@ export default function Home() {
             <div className={style.containerCenter}>
                 <Image src={logoImg} alt="Logo Sujeito Pizzaria"/>
                 <div className={style.login}>
+                    <h1>
+                        Criando sua conta
+                    </h1>
                     <form>
                         <Input placeholder="Digite seu e-mail" type="text"/>
-
+                        <Input placeholder="Digite sua empresa" type="text"/>
                         <Input placeholder="Digite sua senha" type="password"/>
 
                         <Button type="button" loading={false}>
-                            Acessar
+                            Cadastrar
                         </Button>
                     </form>
-                    <Link href="/signup" legacyBehavior>
-                        <a className={style.text}>Não possui uma conta ? Cadastre-se</a>
+                    <Link href="/" legacyBehavior>
+                        <a className={style.text}>Já tenho uma conta ? Acessar</a>
                     </Link>
                 </div>
             </div>
