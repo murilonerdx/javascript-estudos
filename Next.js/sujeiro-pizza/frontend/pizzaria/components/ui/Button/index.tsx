d98cf6ab7c08c0c8a195da2b4a8530e507530060
println <murilo_pereira@terceiros.sicredi.com.br>
2023-04-10 14:04:26 -0300

    Componente do PUTÃO

---
+++ Next.js/sujeiro-pizza/frontend/pizzaria/components/ui/Button/index.tsx
@@ -0,0 +1,27 @@
+import style from './style.module.scss'
+import {ReactNode, ButtonHTMLAttributes} from 'react'
+
+import {FaSpinner} from "react-icons/fa";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    loading?: boolean,
+    childern?: ReactNode
+}
+
+export function Button({loading, children, ...rest}: ButtonProps) {
+
+    return (
+        <button className={style.button} disabled={loading} {...rest}>
+            <a className={style.buttonText}>
+                {loading ? (
+                    <FaSpinner color="#FFF" size={16}/>
+                ) : (
+                    <a className={style.buttonText}>
+                        {children}
+                    </a>
+                )}
+            </a>
+        </button>
+    )
+}
+
