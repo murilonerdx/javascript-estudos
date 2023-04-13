ec79f9e8475cecece105a067357c177a37afe7e5
println <murilo_pereira@terceiros.sicredi.com.br>
2023-04-13 00:04:21 -0300

    fazendo um preview muito loucão para imagem de cadastro de produto

--- Next.js/sujeiro-pizza/frontend/pizzaria/pages/product/index.tsx
+++ Next.js/sujeiro-pizza/frontend/pizzaria/pages/product/index.tsx
@@ -1,9 +1,31 @@
 import {Header} from "../../components/ui/Header";
 import styles from './styles.module.scss'
 import {canSSRAuth} from "../../utils/canSSRAuth";
+import {FiUpload} from "react-icons/fi"
+import {ChangeEvent, useState} from "react";
 
 
 export default function Product() {
+
+    const [avatarUrl, setAvatarUrl] = useState(null)
+    const [imageAvatar, setImageAvatar] = useState(null)
+
+    function handleFile(event: ChangeEvent<HTMLInputElement>){
+        if(!event.target.files){
+            return;
+        }
+
+        const image = event.target.files[0];
+
+        if(!image){
+            return;
+        }
+
+        if(image.type === 'image/jpeg' || image.type === 'image/png'){
+            setAvatarUrl(image)
+            setImageAvatar(URL.createObjectURL(event.target.files[0]))
+        }
+    }
     return (
         <>
             <head>
@@ -15,6 +37,16 @@ export default function Product() {
                 <main className={styles.container}>
                     <h1>Novo Produto</h1>
                     <form className={styles.form}>
+
+                        <label className={styles.labelAvatar}>
+                            <span>
+                                <FiUpload size={25} color="#FFF"/>
+                            </span>
+                            <input type="file" accept="iamge/png, image/jpeg" onChange={handleFile}/>
+                            {avatarUrl && (
+                                <img className={styles.preview} src={imageAvatar} alt="Foto do produto" width={250} height={250}/>
+                            )}
+                        </label>
                         <select>
                             <option>
                                 Pizza
