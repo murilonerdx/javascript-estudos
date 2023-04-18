5be0641d33ff2a06e8f2b1d64820123a0468e312
println <murilo_pereira@terceiros.sicredi.com.br>
2023-04-18 19:55:46 -0300

    Quando o criador arrumar eu volto pqp

--- Next.js/sujeiro-pizza/frontend/pizzaria/pages/dashboard/index.tsx
+++ Next.js/sujeiro-pizza/frontend/pizzaria/pages/dashboard/index.tsx
@@ -1,149 +1,157 @@
-import Head from "next/head"
-import { FiRefreshCcw } from "react-icons/fi"
-import Modal from 'react-modal'
+import React, { useState } from 'react'
+import { canSSRAuth } from '../../utils/canSSRAuth'
+import Head from 'next/head';
+import styles from './styles.module.scss';
 
-import { canSSRAuth } from "../../utils/canSSRAuth"
-import styles from './styles.module.scss'
-import { setupAPIClient } from "../../services/api"
-import { useState } from "react"
+import { FiRefreshCcw } from 'react-icons/fi'
+
+import { setupAPIClient } from '../../services/api'
+
+
+import Modal from 'react-modal';
 import {Header} from "../../components/ui/Header";
 import {ModalOrder} from "../../components/ui/ModalOrder";
 
 type OrderProps = {
-    id: string
-    table: string | number
-    status: boolean
-    draft: boolean
-    name: string | null
+    id: string;
+    table: string | number;
+    status: boolean;
+    draft: boolean;
+    name: string | null;
 }
 
-interface HomeProps {
-    orders: OrderProps[]
+interface HomeProps{
+    orders: OrderProps[];
 }
 
 export type OrderItemProps = {
-    id: string
-    amount: number
-    order_id: string
-    product_id: string
-    product: {
-        id: string
-        name: string
-        description: string
-        price: string | number
-        banner: string
+    id: string;
+    amount: number;
+    order_id: string;
+    product_id: string;
+    product:{
+        id: string;
+        name: string;
+        description: string;
+        price: string;
+        banner: string;
     }
-    order: {
-        id: string
-        table: string | number
-        status: boolean
-        name: string | null
+    order:{
+        id: string;
+        table: string | number;
+        status: boolean;
+        name: string | null;
     }
 }
 
-Modal.setAppElement('#__next')
+export default function Dashboard({ orders }: HomeProps){
 
-export default function Dashboard({ orders }: HomeProps) {
     const [orderList, setOrderList] = useState(orders || [])
-    const [modalItem, setModalItem] = useState<OrderItemProps[]>()
-    const [modalVisible, setModalVisible] = useState(false)
 
-    function handleCloseModal() {
-        setModalVisible(false)
+    const [modalItem, setModalItem] = useState<OrderItemProps[]>()
+    const [modalVisible, setModalVisible] = useState(false);
+
+
+    function handleCloseModal(){
+        setModalVisible(false);
     }
 
-    async function handleOpenModal(id: string) {
-        const api = setupAPIClient()
+    async function handleOpenModalView(id: string){
 
-        const response = await api.get('/order/detail', {
-            params: {
+        const apiClient = setupAPIClient();
+
+        const response = await apiClient.get('/order/detail', {
+            params:{
                 order_id: id,
             }
         })
 
-        setModalItem(response.data)
-        setModalVisible(true)
+        setModalItem(response.data);
+        setModalVisible(true);
+
     }
 
-    async function handleFinishModal(id: string) {
-        const api = setupAPIClient()
 
-        await api.put('/order/finish', {
-            order_id: id
+    async function handleFinishItem(id: string){
+        const apiClient = setupAPIClient();
+        await apiClient.put('/order/finish', {
+            order_id: id,
         })
 
-        const response = await api.get('/order/all')
+        const response = await apiClient.get('/order/all');
 
-        setOrderList(response.data)
-        setModalVisible(false)
+        setOrderList(response.data);
+        setModalVisible(false);
     }
 
-    async function handleRefresh() {
-        const api = setupAPIClient()
 
-        const response = await api.get('/order/all')
+    async function handleRefreshOrders(){
+        const apiClient = setupAPIClient();
 
-        setOrderList(response.data)
+        const response = await apiClient.get('/order/all')
+        setOrderList(response.data);
+
     }
 
-    return (
+    Modal.setAppElement('#__next');
+
+    return(
         <>
             <Head>
-                <title>My-Pizza - Painel</title>
+                <title>Painel - Sujeito Pizzaria</title>
             </Head>
+            <div>
+                <Header/>
 
-            <Header />
+                <main className={styles.container}>
 
-            <main className={styles.container}>
-                <div className={styles.header}>
-                    <h1>Últimos pedidos</h1>
-                    <button onClick={handleRefresh}>
-                        <FiRefreshCcw color="#3fffa3" />
-                    </button>
-                </div>
+                    <div className={styles.containerHeader}>
+                        <h1>Últimos pedidos</h1>
+                        <button onClick={handleRefreshOrders}>
+                            <FiRefreshCcw size={25} color="#3fffa3"/>
+                        </button>
+                    </div>
 
-                <article className={styles.listOrders}>
+                    <article className={styles.listOreders}>
 
-                    {
-                        orderList.length === 0 && (
+                        {orderList.length === 0 && (
                             <span className={styles.emptyList}>
-              Nenhum pedido aberto foi encontrado.
+              Nenhum pedido aberto foi encontrado...
             </span>
-                        )
-                    }
-                    {
-                        orderList.map((item) => (
-                            <section className={styles.orderItem} key={ item.id }>
-                                <button onClick={() => handleOpenModal(item.id)}>
+                        )}
+
+                        {orderList.map( item => (
+                            <section  key={item.id} className={styles.orderItem}>
+                                <button onClick={ () => handleOpenModalView(item.id) }>
                                     <div className={styles.tag}></div>
-                                    <span>Mesa { item.table }</span>
+                                    <span>Mesa {item.table}</span>
                                 </button>
                             </section>
-                        ))
-                    }
+                        ))}
 
-                </article>
-            </main>
+                    </article>
 
-            {
-                modalVisible && (
+                </main>
+
+                { modalVisible && (
                     <ModalOrder
                         isOpen={modalVisible}
+                        onRequestClose={handleCloseModal}
                         order={modalItem}
-                        onClose={handleCloseModal}
-                        onFinish={handleFinishModal}
+                        handleFinishOrder={ handleFinishItem }
                     />
-                )
-            }
+                )}
+
+            </div>
         </>
     )
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+    const apiClient = setupAPIClient(ctx);
 
-    const api = setupAPIClient(ctx)
+    const response = await apiClient.get('/order/all');
 
-    const response = await api.get('/order/all')
 
     return {
         props: {
